34211064ad4472d19b8843f3741b1f64195ff89c
Colin Bertin <48044238+ColinBertin@users.noreply.github.com>
2023-02-28 11:55:55 +0900

    add handler search bar

---
+++ components/SearchBar.tsx
@@ -0,0 +1,34 @@
+import { ChangeEvent } from "react";
+import { BiSearch } from "react-icons/bi";
+
+type SearchBarProps = {
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  getDefintion: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+  word: string;
+};
+
+export default function SearchBar({
+  handleChange,
+  getDefintion,
+  word,
+}: SearchBarProps) {
+  return (
+    <form className="flex flex-col md:flex-row justify-center items-center content-around gap-2">
+      <input
+        type="text"
+        id="first_name"
+        placeholder="Hello"
+        onChange={(e) => handleChange(e)}
+        className="border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 w-3/4"
+      />
+      <button
+        type="submit"
+        onClick={(e) => getDefintion(e)}
+        disabled={!word}
+        className="max-w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        <BiSearch size={20} />
+      </button>
+    </form>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,34 @@
 import Head from "next/head";
 // import { Inter } from "next/font/google";
 import NavBar from "@/components/NavBar";
-import { useState } from "react";
+import SearchBar from "@/components/SearchBar";
+import { SetStateAction, useState } from "react";
 import clsx from "clsx";
 
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
+  const [word, setWord] = useState("");
 
   const handleDarkMode = () => {
     setDarkMode((mode) => !mode);
+  };
+
+  const handleChange = (e: { target: { value: SetStateAction<string> } }) => {
+    setWord(e.target.value);
+  };
+
+  const getDefintion = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    try {
+      const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -22,6 +40,11 @@ export default function Home() {
       </Head>
       <main>
         <NavBar darkMode={darkMode} handleDarkMode={handleDarkMode} />
+        <SearchBar
+          handleChange={handleChange}
+          getDefintion={getDefintion}
+          word={word}
+        />
       </main>
     </div>
   );
