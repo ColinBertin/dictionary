983c07986dd43d07d5ff6cbff6cda159cb5c21df
Colin Bertin <48044238+ColinBertin@users.noreply.github.com>
2023-02-28 11:18:06 +0900

    set navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,37 @@
-import { BsBookHalf } from "react-icons/bs";
+import { BsBookHalf, BsSunFill, BsMoonFill } from "react-icons/bs";
+import { Switch } from "@headlessui/react";
+import { clsx } from "clsx";
 
-export default function NavBar() {
+type NavBarProps = {
+  darkMode: boolean;
+  handleDarkMode: () => void;
+};
+
+export default function NavBar({ darkMode, handleDarkMode }: NavBarProps) {
   return (
-    <nav>
-      <BsBookHalf />
+    <nav className="flex justify-between py-4 px-6">
+      <BsBookHalf size={24} color={clsx(darkMode ? "red" : "green")} />
+      <div className="flex gap-4">
+        <Switch
+          checked={darkMode}
+          onChange={handleDarkMode}
+          className={`${
+            darkMode ? "bg-blue-600" : "bg-gray-200"
+          } relative inline-flex h-6 w-11 items-center rounded-full`}
+        >
+          <span className="sr-only">Enable notifications</span>
+          <span
+            className={`${
+              darkMode ? "translate-x-6" : "translate-x-1"
+            } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+          />
+        </Switch>
+        {darkMode ? (
+          <BsSunFill size={24} color={"white"} />
+        ) : (
+          <BsMoonFill size={20} />
+        )}
+      </div>
     </nav>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import NavBar from "@/components/NavBar";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleDarkMode = () => {
+    setDarkMode((mode) => !mode);
+  };
+
   return (
     <div className="h-screen w-screen">
       <Head>
@@ -13,7 +20,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <NavBar />
+        <NavBar darkMode={darkMode} handleDarkMode={handleDarkMode} />
       </main>
     </div>
   );
