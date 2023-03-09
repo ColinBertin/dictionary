7bec23c3697f21c4bcb37eae95b7faa1e47970e8
Colin Bertin <48044238+ColinBertin@users.noreply.github.com>
2023-03-09 19:26:47 +0900

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "My Dictionary",
+};
+
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
