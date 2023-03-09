94e516629f36792ad80b5ff1741eb7187d7ff4b1
Colin Bertin <48044238+ColinBertin@users.noreply.github.com>
2023-03-09 19:34:36 +0900

    add capitalize func

--- components/Card.tsx
+++ components/Card.tsx
@@ -10,13 +10,18 @@ type CardProps = {
 };
 
 export default function Card({ data, darkMode }: CardProps) {
+  function capitalize(string: string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   useEffect(() => {
     console.log(data);
   }, [data]);
+
   return (
     <div className={clsx(darkMode ? "text-white" : "", "px-24 py-28")}>
       <header className="flex justify-around">
-        <h1 className="text-8xl">{data.word}</h1>
+        <h1 className="text-8xl">{capitalize(data.word)}</h1>
         <audio controls>
           <source src={data.phonetics[0].audio} type="audio/mp3" />
           Your browser does not support the audio tag.
